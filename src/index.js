b09e129512f450e60d324a704dbedc6e9012e90f
Satoko Gruson <satokogruson@gmail.com>
2024-04-13 17:32:27 +0200

    updated api key

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "10b545o25teaa28dd38fd076fc778f2c";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
